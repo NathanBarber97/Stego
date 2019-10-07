e7c5d8d080e1bb2326f41202f7658c1415d85152
Nathan Barber <nbarber789@gmail.com>
2019-10-06 20:15:30 -0700

    Removed prints

--- dcstego.py
+++ dcstego.py
@@ -1,20 +1,13 @@
-import os
 import base64
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from bitstring import ConstBitStream
-from bitstring import BitArray
-from binascii import a2b_hex, b2a_uu
 
 def hidemsg(coverImg, hiddenMsg, header, password):
     fullMsg = b"@header" + header + b"@message" + hiddenMsg + b"@end"
     cipherText = encrypt(fullMsg, password.encode()) + b'@!590@'
-    #print(cipherText)
-    #cipherText = fullMsg
-    if len(cipherText)*8 > len(coverImg):
-        print("hidden too big!!!")
     bitstream = ConstBitStream(bytes=cipherText)
     stegoImg = b""
     byteCount  = 0
@@ -22,11 +15,8 @@ def hidemsg(coverImg, hiddenMsg, header, password):
         byteCount += 1
         if byteCount < bitstream.length:
             newBit = bitstream.read("bits:1").bin
-            # print("new bit" + str(newBit))
             newByte = setbit(byte, newBit)
-            # print("new byte" + str(bytes(newByte)))
             stegoImg += newByte.to_bytes(1, byteorder='big')
-            # print("stegoImg" + str(stegoImg))
         else:
             stegoImg += byte.to_bytes(1, byteorder='big')
 
@@ -36,12 +26,10 @@ def extractmsg(stegoImg, password):
     hiddenBits = ''
     for byte in stegoImg:
         hiddenBits += bin(byte)[-1]
-    #print(hiddenBits)
     encryptedMsg = convertBitstoBytes(hiddenBits)
     index = encryptedMsg.find('@!590')
     if index == -1:
         print ("@!590 not found")
-    #print(encryptedMsg[:index].encode())
     hiddenBytes = (decrypt(encryptedMsg[:index].encode(), password.encode()))
     hiddenMsg = hiddenBytes.decode("utf-8","replace")
     headerIndex = hiddenMsg.find('@header')
@@ -52,10 +40,8 @@ def extractmsg(stegoImg, password):
 def convertBitstoBytes(bits):
     byteChunks = [bits[i:i+8] for i in range(0, len(bits), 8)]
     bytes = ''
-    #print(byteChunks)
     for byteChunk in byteChunks:
         bytes += chr(int(byteChunk, 2))
-    #print(bytes)
     return bytes
 
 def setbit(byte, bit):
