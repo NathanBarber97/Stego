f03b36a049147b3c6076ea82270813f18b6b019c
Nathan Barber <nbarber789@gmail.com>
2019-10-06 20:40:12 -0700

    Added comments

--- dcstego.py
+++ dcstego.py
@@ -5,14 +5,31 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from bitstring import ConstBitStream
 
+# ----------------------------------------------------------------------------------------------------------------------
+# The hidemsg function hides a header and a hidden message in the lowest bit of the bytes of an image, after
+# encrypting them
+#
+# Params:
+# coverImg - an array of bytes that represents an image
+# hiddenMsg - an array of bytes that represents a hidden message
+# header - a header describing the hidden message
+# password - the password used to encrypt the message
+#
+# Returns:
+# hidemsg returns an array of bytes that represents a new image with the message hidden inside
+# ----------------------------------------------------------------------------------------------------------------------
 def hidemsg(coverImg, hiddenMsg, header, password):
+    #add separators to message and encrypt it
     fullMsg = b"@header" + header + b"@message" + hiddenMsg + b"@end"
     cipherText = encrypt(fullMsg, password.encode()) + b'@!590@'
+    #turn the encrypted message into bits
     bitstream = ConstBitStream(bytes=cipherText)
+    #new image bytes
     stegoImg = b""
     byteCount  = 0
     for byte in coverImg:
         byteCount += 1
+        #make sure there are still bits to store
         if byteCount < bitstream.length:
             newBit = bitstream.read("bits:1").bin
             newByte = setbit(byte, newBit)
@@ -22,6 +39,17 @@ def hidemsg(coverImg, hiddenMsg, header, password):
 
     return stegoImg
 
+# ----------------------------------------------------------------------------------------------------------------------
+# The extractmsg function extracts the header and the hidden message from the lowest bit of the bytes of an image, and
+# decrypts them
+#
+# Params:
+# stegoImg - an array of bytes that represents an image that has a hidden message
+# password - the password used to decrypt the message
+#
+# Returns:
+# extractmessage returns a string containing the header and an array of bytes that represents the hidden message
+# ----------------------------------------------------------------------------------------------------------------------
 def extractmsg(stegoImg, password):
     hiddenBits = ''
     for byte in stegoImg:
@@ -37,6 +65,15 @@ def extractmsg(stegoImg, password):
     endIndex = hiddenMsg.find('@end')
     return hiddenMsg[headerIndex + 7 : messageIndex], hiddenBytes[messageIndex + 8 : endIndex]
 
+# ----------------------------------------------------------------------------------------------------------------------
+# The convertBitstoBytes function converts an array of bits into an array of bytes
+#
+# Params:
+# bits - the array of bits to be converted
+#
+# Returns:
+# convertBitstoBytes returns the array of bytes
+# ----------------------------------------------------------------------------------------------------------------------
 def convertBitstoBytes(bits):
     byteChunks = [bits[i:i+8] for i in range(0, len(bits), 8)]
     bytes = ''
@@ -44,12 +81,32 @@ def convertBitstoBytes(bits):
         bytes += chr(int(byteChunk, 2))
     return bytes
 
+# ----------------------------------------------------------------------------------------------------------------------
+# The setbit function sets the lowest bit of a byte and returns the new byte
+#
+# Params:
+# byte - the old byte that is to be modified
+# bit - the new bit to be stored in the lowest bit of the byte
+#
+# Returns:
+# setbit returns the byte with the last bit modified
+# ----------------------------------------------------------------------------------------------------------------------
 def setbit(byte, bit):
     newByte = list(bin(byte))
     newByte[-1] = bit
     return int(''.join(newByte), 2)
 
-
+# ----------------------------------------------------------------------------------------------------------------------
+# The encrypt function takes in a message and encrypts using the Fernet encryption algorithm from the cryptography
+# package
+#
+# Params:
+# hiddenMsg - the plaintext to be encrypted (in bytes)
+# password - the password used to encrypt the message (in bytes)
+#
+# Returns:
+# encrypt returns an array of bytes that represents the encrypted message
+# ----------------------------------------------------------------------------------------------------------------------
 def encrypt(hiddenMsg, password):
     salt = b'?%U;\x97\xd8%\x8c\x08\xae\xdeL\xae\xba\xa5M'
     kdf = PBKDF2HMAC(
@@ -65,6 +122,17 @@ def encrypt(hiddenMsg, password):
     token = encrypter.encrypt(hiddenMsg)
     return token
 
+# ----------------------------------------------------------------------------------------------------------------------
+# The decrypt function takes in ciphertext and decrypts it using the Fernet encryption algorithm from the cryptography
+# package
+#
+# Params:
+# ciphertext - the ciphertext to be decrypted (in bytes)
+# password - the password used to decrypt the message (in bytes)
+#
+# Returns:
+# decrypt returns an array of bytes that represents the decrypted message
+# ----------------------------------------------------------------------------------------------------------------------
 def decrypt(cipherText, password):
     salt = b'?%U;\x97\xd8%\x8c\x08\xae\xdeL\xae\xba\xa5M'
     kdf = PBKDF2HMAC(
